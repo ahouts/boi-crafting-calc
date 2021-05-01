3896e68a44339e538b9d05a98ed64de072d78b0c
Andrew Houts <ahouts4@gmail.com>
2021-05-01 14:09:55 -0500

    move scripts to package.json

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-wasm-pack build --target web --release
-npx webpack --config webpack.config.js --mode production
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-wasm-pack test --chrome --headless
